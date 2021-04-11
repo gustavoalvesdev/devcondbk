fabe6908c522638a5d9e0ed3980d7271ad55d833
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-04-11 14:38:39 -0300

    /foundandlost (get)

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -4,7 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\FoundAndLost;
+
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+
+        $lost = FoundAndLost::where('status', 'LOST')
+        ->orderBy('datecreated', 'ASC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+        $recovered = FoundAndLost::where('status', 'RECOVERED')
+        ->orderBy('datecreated', 'ASC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+        foreach($lost as $lostKey => $lostValue) {
+            $lost[$lostKey]['datecreated'] = date('d/m/Y', strtotime($lostValue['datecreated']));
+            $lost[$lostKey]['photo'] = asset('storage/'.$lostValue['photo']);
+        }
+
+        foreach($recovered as $recKey => $recValue) {
+            $recovered[$recKey]['datecreated'] = date('d/m/Y', strtotime($recValue['datecreated']));
+            $recovered[$recKey]['photo'] = asset('storage/'.$recValue['photo']);
+        }
+
+        $array['lost'] = $lost;
+        $array['recovered'] = $recovered;
+
+        return $array;
+    }
 }
