a4758d07088a1c62a28cbcc9cc693834028bd10d
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-05-19 17:07:28 -0300

    /reservations 2

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Area;
+use phpDocumentor\Reflection\DocBlock\Tags\InvalidTag;
 
 class ReservationController extends Controller
 {
     public function getReservations()
     {
-        $array = ['error' => ''];
+        $array = ['error' => '', 'list' => []];
         $daysHelper = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
 
         // allowed areas for reservation
@@ -27,16 +28,52 @@ class ReservationController extends Controller
             array_shift($dayList);
 
             // Adding relevant days
+            foreach ($dayList as $day) {
+                if (intval($day) != $lastDay + 1) {
+                    $dayGroups[] = $daysHelper[$lastDay];
+                    $dayGroups[] = $daysHelper[$day];
+                }
+
+                $lastDay = intval($day);
+            }
 
             // Adding the last day
             $dayGroups[] = $daysHelper[end($dayList)];
 
-            echo 'Area: ' . $area['title'] . "\n";
-            print_r($dayGroups);
-            echo "\n-------------";
+            // Joining dates (day1 - day2)
+            $dates = '';
+            $close = 0;
+
+            foreach ($dayGroups as $group) {
+                if ($close === 0) {
+                    $dates .= $group;
+                } else {
+                    $dates .= '-' . $group . ',';
+                }
+
+                $close = 1 - $close;
+            }
+
+            $dates = explode(',', $dates);
+            array_pop($dates);
+
+            // adding time
+            $start = date('H:i', strtotime($area['start_time']));
+            $end = date('H:i', strtotime($area['end_time']));
+
+            foreach ($dates as $dKey => $dValue) {
+                $dates[$dKey] .= ' ' . $start . ' às ' . $end;
+            }
+
+            $array['list'][] = [
+                'id' => $area['id'],
+                'cover' => asset('storage/' . $area['cover']),
+                'title' => $area['title'],
+                'dates' => $dates
+            ];
         }
 
-        $array['list'] = $areas;
+
 
         return $array;
     }
