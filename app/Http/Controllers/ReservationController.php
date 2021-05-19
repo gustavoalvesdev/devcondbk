bd34ec8d700d663cba69c0b77edfb24446217e5e
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-05-19 16:47:28 -0300

    /reservations 1

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,7 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Area;
+
 class ReservationController extends Controller
 {
-    //
+    public function getReservations()
+    {
+        $array = ['error' => ''];
+        $daysHelper = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
+
+        // allowed areas for reservation
+        $areas = Area::where('allowed', 1)->get();
+
+        foreach ($areas as $area) {
+            $dayList = explode(',', $area['days']);
+
+            $dayGroups = [];
+
+            // Adding the first day
+            $lastDay = intval(current($dayList));
+            $dayGroups[] = $daysHelper[$lastDay];
+            array_shift($dayList);
+
+            // Adding relevant days
+
+            // Adding the last day
+            $dayGroups[] = $daysHelper[end($dayList)];
+
+            echo 'Area: ' . $area['title'] . "\n";
+            print_r($dayGroups);
+            echo "\n-------------";
+        }
+
+        $array['list'] = $areas;
+
+        return $array;
+    }
 }
